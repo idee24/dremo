268b16b1fe6e9ed1a732e81b9742b8fd0acbc246
blue-chip <idee1140@gmail.com>
2018-09-08 10:05:43 +0100

    migration

---
+++ app/src/main/java/momatechsoftwares/rest_demo/activities/CustomActivity.java
@@ -0,0 +1,15 @@
+package momatechsoftwares.rest_demo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import momatechsoftwares.rest_demo.R;
+
+public class CustomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom);
+    }
+}
